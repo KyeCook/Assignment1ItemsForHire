63fdf414100dc20e6178295b41ce5b281c0f2fc7
KyeCook <acfanatics@hotmail.com>
2016-03-29 22:04:08 +1000

    Initial commit - Basic Pseudocode for majority of functions semi-complete

---
+++ itemsForHire.py
@@ -0,0 +1,40 @@
+"""
+CP1404 - Assignment 1 :2016
+Items For Hire
+Kye Cook
+29/03/2016
+
+Pseudocode:
+
+Function Main()
+    call load_items()
+    set load_items() as tuple variable called items
+    display welcome message
+    display MENU
+    get choice
+    while choice is not 'Q'
+        if choice is 'L'
+            display items
+        else if choice is 'H'
+            call hire_items()
+        else if choice is 'R'
+            call return_items()
+        else if choice = 'A'
+            call add_items()
+        else
+            display error message
+        display MENU
+        get choice
+    print display thank you message and number of items saved into list
+
+Function load_items()
+    load items.csv document
+    convert into tuple (?here or in main?)
+    return loaded document
+
+Function hire_items()
+
+Function return_items()
+
+Function add_items()
+"""
